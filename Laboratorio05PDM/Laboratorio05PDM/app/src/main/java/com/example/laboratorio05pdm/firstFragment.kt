da76c3ef7a9400d6ed77ba6b7d9feae37276333b
RicardoLopez <00107521@uca.edu.sv>
2023-04-17 21:52:39 -0600

    Laboratorio 05

---
+++ Laboratorio05PDM/Laboratorio05PDM/app/src/main/java/com/example/laboratorio05pdm/firstFragment.kt
@@ -0,0 +1,32 @@
+package com.example.laboratorio05pdm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+class firstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.add_movie_button).setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_thirdFragment)
+        }
+        view.findViewById<CardView>(R.id.card_view1).setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
---
+++ Laboratorio05PDM/Preguntas Labo 05.md
@@ -0,0 +1,14 @@
+# Preguntas Laboratorio 05
+
+1. ¿Qué diferencia existe entre navegar dentro de la aplicación utilizando  
+Navigation Component o hacer uso de intents?
+R/ Navigation Component simplifica el proceso de navegación en una aplicación, proporciona una forma declarativa de definir rutas de navegación entre varias pantallas, llamadas "destinos", se encargara de gestionar todo el proceso de navegación, incluyendo transiciones entre fragmentos, animaciones y navegación hacia atrás, mientras que en los intents son objetos utilizados para comunicar información entre los componentes de una aplicación.
+
+2. Menciona los componentes fundamentales de Navigation  
+Component
+R/ Navigation Graph
+	 NavHost
+	 NavController
+
+3. Mencione algunas de las ventajas del Navigation Graph 
+R/ Es una manera sencilla de estructurar la navegación de nuestra aplicación móvil, ya que su interfaz permite hacer las conexiones entre los destinos.
