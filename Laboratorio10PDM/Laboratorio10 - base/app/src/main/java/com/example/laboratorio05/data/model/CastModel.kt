22f41e6c59bf5fa20155afd218d11be428217686
RicardoLopez00107521 <00107521@uca.edu.sv>
2023-05-29 14:43:31 -0600

    Laboratorio10

---
+++ Laboratorio10PDM/Laboratorio10 - base/app/src/main/java/com/example/laboratorio05/data/model/CastModel.kt
@@ -0,0 +1,14 @@
+package com.example.laboratorio05.data.model
+
+import androidx.room.Entity
+
+@Entity(tableName = "cast_table", primaryKeys = ["movieId", "actorId"])
+data class CastModel(
+    val movieId: Int,
+    val actorId: Int
+)
+
+{
+    constructor():
+            this(0,0)
+}
